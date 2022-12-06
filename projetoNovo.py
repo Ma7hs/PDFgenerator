abf73bcb8db76af814b3764e7832aa8b0e42dd63
Matheus Siqueira Silva <87048682+Ma7hs@users.noreply.github.com>
2022-12-06 08:29:36 -0300

    Update projetoNovo.py

--- projetoNovo.py
+++ projetoNovo.py
@@ -1,3 +1,5 @@
+!pip install FPDF
+
 from fpdf import FPDF
 
 descriptionProject = input("Digite a descrição do projeto: ")
@@ -9,7 +11,15 @@ estimatedTotalValue = int(hoursProject) * int(hoursValue)
 newPdf = FPDF()
 newPdf.add_page()
 newPdf.set_font("Arial")
-newPdf.image("modeloOrcamento.png")
+newPdf.image("template.png", x=0, y=0)
+
+
+newPdf.text(115, 147, descriptionProject)
+newPdf.text(115, 161, hoursProject)
+newPdf.text(115, 176, hoursValue)
+newPdf.text(115, 191, prizeProject)
+newPdf.text(115, 206, str(estimatedTotalValue))
+
 
 newPdf.output("Orçamento.pdf")
 print("PDF gerado com sucesso!")
